2ef18ee451428d92082a7871de50a639e59a70b4
Fogarasi Andras <fogarasiandras14@gmail.com>
2024-04-30 23:00:28 +0300

    Finish sql file

--- server/db/create_tables.sql
+++ server/db/create_tables.sql
@@ -1,12 +1,12 @@
 USE works_for_students;
-DROP TABLE IF EXISTS banned;
 DROP TABLE IF EXISTS guidance;
 DROP TABLE IF EXISTS notification;
 DROP TABLE IF EXISTS problem;
 DROP TABLE IF EXISTS application;
-DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS job_category;
-DROP TABLE IF EXISTS admins;
+DROP TABLE IF EXISTS spoken_language;
+DROP TABLE IF EXISTS language;
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS company;
@@ -16,12 +16,7 @@ CREATE TABLE IF NOT EXISTS company (
     company_name VARCHAR(50),
     location VARCHAR(60) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    rating INT DEFAULT 0
-);
-
-CREATE TABLE IF NOT EXISTS language (
-	language_id INT AUTO_INCREMENT PRIMARY KEY,
-	language_name VARCHAR(30),
+    rating INT DEFAULT 0        -- lenne egy ful, ahol lehet ertekelni a ceget
 );
 
 CREATE TABLE IF NOT EXISTS user (
@@ -34,19 +29,25 @@ CREATE TABLE IF NOT EXISTS user (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     flag VARCHAR(10) NOT NULL,
 	company_id INT,
-	FOREIGN KEY (company_id) REFERENCES company(company_id)
+	banned BOOLEAN DEFAULT FALSE,
+	FOREIGN KEY (company_id) REFERENCES company(id)
 );
 
 CREATE TABLE IF NOT EXISTS student (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 user_id INT UNIQUE,
-	 university_name VARCHAR(30),
-	 year INT NOT NULL,
-	 cv_filename VARCHAR(30),   -- file_name
-	 mother_tongue VARCHAR(30),
-	 experience TEXT,
-	 description TEXT,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT UNIQUE,
+	university_name VARCHAR(30),
+	year INT NOT NULL,
+	cv_filename VARCHAR(30),   -- valahol a file-ok kozott a cv-ket
+	mother_tongue VARCHAR(30),
+	experience TEXT,   -- leirasok eddigi tapasztalatokrol
+	presentation TEXT,   -- bemutatkozo szoveg
     FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS language (
+	language_id INT AUTO_INCREMENT PRIMARY KEY,
+	language_name VARCHAR(30)
 );
 
 CREATE TABLE IF NOT EXISTS spoken_language (
@@ -59,64 +60,59 @@ CREATE TABLE IF NOT EXISTS spoken_language (
 );
 
 CREATE TABLE IF NOT EXISTS job_category (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 category_name VARCHAR(50),
-	 description Text
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	category_name VARCHAR(50),
+	description TEXT
 );
 
-CREATE TABLE IF NOT EXISTS post (
+CREATE TABLE IF NOT EXISTS job (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    description TEXT,
-    requirements TEXT,
-    salary INT,
+    description TEXT,    -- altalanos munkaleiras
+    requirements TEXT,   -- elvarasok
+    salary INT NULL,
     company_id INT NOT NULL,
-    category_id INT,
-    job_time VARCHAR(20) NOT NULL,
+    category_id INT,  
+	working_hours VARCHAR(20) NOT NULL,  -- hany orat kene dolgozni (hetente)
+	application_limit INT,
     FOREIGN KEY (company_id) REFERENCES company(id),
     FOREIGN KEY (category_id) REFERENCES job_category(id)
 );
 
 CREATE TABLE IF NOT EXISTS application (
-	 date_application TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	 student_id INT NOT NULL,
-	 post_id INT NOT NULL,
-	 accept BOOLEAN,
-	 PRIMARY KEY (student_id, post_id),
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	date_application TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	student_id INT NOT NULL,
+	job_id INT NOT NULL,
+	accept BOOLEAN,
+	UNIQUE KEY (student_id, job_id),
     FOREIGN KEY (student_id) REFERENCES student(id),
-    FOREIGN KEY (post_id) REFERENCES post(id)
+    FOREIGN KEY (job_id) REFERENCES job(id)
 );
 
 CREATE TABLE IF NOT EXISTS problem (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 user_id INT NOT NULL,
-	 description TEXT,
-	 solved BOOLEAN,
-	 FOREIGN KEY (user_id) REFERENCES user(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	description TEXT,
+	solved BOOLEAN,
+	FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE IF NOT EXISTS notification (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 message TEXT, 
-	 from_member INT NOT NULL,
-	 to_member INT NOT NULL,
-	 FOREIGN KEY (from_member) REFERENCES user(id),
-	 FOREIGN KEY (to_member) REFERENCES user(id)
+CREATE TABLE IF NOT EXISTS notification (  -- valaszok a cegektol (hr-tol)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	message TEXT, 
+	from_member INT NOT NULL,
+	to_member INT NOT NULL,
+	FOREIGN KEY (from_member) REFERENCES user(id),
+	FOREIGN KEY (to_member) REFERENCES user(id)
 );
 	 
-CREATE TABLE IF NOT EXISTS guidance (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 text TEXT NOT NULL,
-	 file_name VARCHAR(20),
-	 company_id INT NOT NULL,
-	 FOREIGN KEY(company_id) REFERENCES company(id)
+CREATE TABLE IF NOT EXISTS guidance (  -- works_for_studentscegnek kuldeni valamit
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	text TEXT NOT NULL,
+	file_name VARCHAR(20),
+	company_id INT NOT NULL,
+	FOREIGN KEY(company_id) REFERENCES company(id)
 );
 
-CREATE TABLE IF NOT EXISTS banned (
-	 id INT AUTO_INCREMENT PRIMARY KEY,
-	 description TEXT,
-	 user_id INT NOT NULL,
-	 FOREIGN KEY(user_id) REFERENCES user(id)
-);
-
-SHOW TABLES;
+SHOW TABLES;works_for_studentsworks_for_students
