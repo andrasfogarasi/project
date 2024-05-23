4f0a3c82a7f54cfa3b51e0e7734c950668bed754
Fogarasi Andras <fogarasiandras14@gmail.com>
2024-05-24 00:53:08 +0300

    Fix webpack error

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from "./Components/LoginForm/LoginForm";
 import RegisterForm from "./Components/RegisterForm/RegisterForm";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
     </Router>
   );
 };
-
 
 export default App;
