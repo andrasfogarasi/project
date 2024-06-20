8d278c03c4d978cc287c972f36cb12e35ab3e5ac
Fogarasi Andras <fogarasiandras14@gmail.com>
2024-06-20 15:29:49 +0300

    add tables

--- server/db/create_tables.js
+++ server/db/create_tables.js
@@ -1,4 +1,4 @@
-import database_connection from './database_connection';
+import database_connection from "./database_connection";
 
 export const createTable = async () => {
     try {
@@ -8,7 +8,7 @@ export const createTable = async () => {
         company_name VARCHAR(50),
         location VARCHAR(60) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        rating INT DEFAULT 0        -- lenne egy ful, ahol lehet ertekelni a ceget
+        rating INT DEFAULT 0    
     );
     `);
 
@@ -34,29 +34,99 @@ export const createTable = async () => {
         user_id INT UNIQUE,
         university_name VARCHAR(30),
         year INT NOT NULL,
-        cv_filename VARCHAR(30),   -- valahol a file-ok kozott a cv-ket
+        cv_filename VARCHAR(30),  
         mother_tongue VARCHAR(30),
-        experience TEXT,   -- leirasok eddigi tapasztalatokrol
-        presentation TEXT,   -- bemutatkozo szoveg
+        experience TEXT,  
+        presentation TEXT,  
         FOREIGN KEY (user_id) REFERENCES user(id)
     );
         `);
 
         await database_connection.executeQuery(`
-            CREATE TABLE IF NOT EXISTS student (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            user_id INT UNIQUE,
-            university_name VARCHAR(30),
-            year INT NOT NULL,
-            cv_filename VARCHAR(30),   -- valahol a file-ok kozott a cv-ket
-            mother_tongue VARCHAR(30),
-            experience TEXT,   -- leirasok eddigi tapasztalatokrol
-            presentation TEXT,   -- bemutatkozo szoveg
-            FOREIGN KEY (user_id) REFERENCES user(id)
-        );
+        CREATE TABLE IF NOT EXISTS language (
+        language_id INT AUTO_INCREMENT PRIMARY KEY,
+        language_name VARCHAR(30)
+    );
             `);
 
+        await database_connection.executeQuery(`
+    CREATE TABLE IF NOT EXISTS spoken_language (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	student_id INT,
+	language_id INT,
+	level VARCHAR(3),
+	FOREIGN KEY (student_id) REFERENCES user(id),
+	FOREIGN KEY (language_id) REFERENCES language(language_id)
+    );
+    `);
 
+        await database_connection.executeQuery(`
+        CREATE TABLE IF NOT EXISTS job_category (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        category_name VARCHAR(50),
+        description TEXT
+    );
+    `);
+
+        await database_connection.executeQuery(`
+            CREATE TABLE IF NOT EXISTS job (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        description TEXT,    
+        requirements TEXT,   
+        salary INT NULL,
+        company_id INT NOT NULL,
+        category_id INT,  
+        working_hours VARCHAR(20) NOT NULL,  
+        application_limit INT,
+        FOREIGN KEY (company_id) REFERENCES company(id),
+        FOREIGN KEY (category_id) REFERENCES job_category(id)
+        );
+    `);
+
+        await database_connection.executeQuery(`
+        CREATE TABLE IF NOT EXISTS problem (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL,
+        description TEXT,
+        solved BOOLEAN,
+        FOREIGN KEY (user_id) REFERENCES user(id)
+    );
+    `);
+
+        await database_connection.executeQuery(`
+        CREATE TABLE IF NOT EXISTS notification (  
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        message TEXT, 
+        from_member INT NOT NULL,
+        to_member INT NOT NULL,
+        FOREIGN KEY (from_member) REFERENCES user(id),
+        FOREIGN KEY (to_member) REFERENCES user(id)
+    );
+    `);
+
+        await database_connection.executeQuery(`
+        CREATE TABLE IF NOT EXISTS guidance (  
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        text TEXT NOT NULL,
+        file_name VARCHAR(20),
+        company_id INT NOT NULL,
+        FOREIGN KEY(company_id) REFERENCES company(id)
+    );
+    `);
+
+        await database_connection.executeQuery(`
+        CREATE TABLE IF NOT EXISTS application (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        date_application TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        student_id INT NOT NULL,
+        job_id INT NOT NULL,
+        accept BOOLEAN,
+        UNIQUE KEY (student_id, job_id),
+        FOREIGN KEY (student_id) REFERENCES student(id),
+        FOREIGN KEY (job_id) REFERENCES job(id)
+    );
+    `);
     } catch (err) {
         console.error(`Hiba a tablak letrehozasanal!: ${err}`);
         process.exit(1);
