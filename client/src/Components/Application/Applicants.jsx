349ce2608d4323c4fb80c6001919643cdb1cc368
Fogarasi Andras <fogarasiandras14@gmail.com>
2024-08-05 00:08:04 +0300

    make a better css

--- client/src/Components/Application/Applicants.jsx
+++ client/src/Components/Application/Applicants.jsx
@@ -61,7 +61,6 @@ const Applicants = () => {
           "Content-Type": "application/json",
         },
 
-        body: JSON.stringify(formData),
         credentials: "include",
       });
 
--- client/src/Components/Error/NotFoundPage.jsx
+++ client/src/Components/Error/NotFoundPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../Headers/Header.jsx";
+import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
   return (
-    <div className="container">
-      <h1 className="header">404</h1>
-      <p className="text">Page Not Found</p>
-      <Link to="/" className="link">
-        Go Back Home
-      </Link>
+    <div>
+      <Header />
+      <div className="container">
+        <h1 className="header">404</h1>
+        <p className="text">Page Not Found</p>
+        <Link to="/" className="button">
+          Go Back Home
+        </Link>
+      </div>
     </div>
   );
 };
